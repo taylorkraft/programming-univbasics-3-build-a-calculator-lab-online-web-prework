c873c6502b82a2c6a691243b59e2e5db8731056f
Taylor Kraft <taylorkraft16@gmail.com>
2019-10-19 01:59:37 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,26 @@
-def addition(num1, num2)
+num1 = 5
+num2 = 8
 
+def addition(num1, num2)
+return num1 + num2
 end
 
 def subtraction(num1, num2)
-
+return num1 - num2
 end
 
 def division(num1, num2)
-
+return num1 / num2
 end
 
 def multiplication(num1, num2)
-
+return num1 * num2
 end
 
 def modulo(num1, num2)
-
+return Math:modulo num1 / num2
 end
 
 def square_root(num)
-
+return Math.sqrt num
 end
